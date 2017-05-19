41c6956231312d7d46f6547af5ded4b3b361fe24
Ross Goodwin <ross.goodwin@gmail.com>
2017-05-19 17:14:37 -0400

    improvements

--- js/sketch.js
+++ js/sketch.js
@@ -13,6 +13,8 @@ var addingCaption = false;
 
 var canvasSize = 700;
 
+var mouseSwitch = false;
+
 function preload() {
 	imgPathList = loadJSON('./data/img_paths.json');
 }
@@ -35,7 +37,7 @@ function draw() {
 	background(238,238,238);
 	image(curImg, 0, 0);
 
-	if (mouseIsPressed && mouseX <= canvasSize && mouseY <= canvasSize) {
+	if (mouseIsPressed && mouseX <= canvasSize && mouseY <= canvasSize && mouseSwitch) {
 		rect(mouseOriginX, mouseOriginY, mouseX, mouseY);
 	}
 	else if (addingCaption) {
@@ -46,9 +48,12 @@ function draw() {
 }
 
 function mousePressed() {
+	mouseSwitch = false;
 	if (mouseX <= canvasSize && mouseY <= canvasSize) {
+		addingCaption = false;
 		mouseOriginX = mouseX;
 		mouseOriginY = mouseY;
+		mouseSwitch = true;
 	}
 }
 
@@ -57,6 +62,7 @@ function mouseReleased() {
 		addingCaption = true;
 		mouseDestX = mouseX;
 		mouseDestY = mouseY;
+		$("#text-input").focus();
 	}
 }
 
@@ -80,15 +86,31 @@ $(function(){
 		var coordStr = coords.join(',');
 		var imgFp = imgPathList[imgPathIndex];
 		// console.log(cap);
-		if (coordStr in captions[imgFp]) {
-			captions[imgFp][coordStr].push(cap);
+
+		if ( coordStr === '0,0,0,0' || mouseOriginX == mouseDestX || mouseOriginY == mouseDestY ) {
+			alert("No box selected!");
+		}
+		else if (cap === '') {
+			alert("No caption!");
 		}
 		else {
-			captions[imgFp][coordStr] = [cap];
-		}
-		
+			if (coordStr in captions[imgFp]) {
+				captions[imgFp][coordStr].push(cap);
+			}
+			else {
+				captions[imgFp][coordStr] = [cap];
+			}
+			$('#cap-container').prepend('<p>'+imgFp+' | '+coordStr+' | '+cap+'</p>');
 
-		$('#cap-container').prepend('<p>'+imgFp+' | '+coordStr+' | '+cap+'</p>');
+			$('#text-input').val('');
+		}
+
+		addingCaption = false;
+
+		mouseOriginX = 0;
+		mouseOriginY = 0;
+		mouseDestX = 0;
+		mouseDestY = 0;
 	});
 
 	$('#download-button').click(function(){
